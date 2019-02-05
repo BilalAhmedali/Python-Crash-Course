abbf30dc536c53d9ca84e94fe4e1fb9a84cdcf2f
= <roll94@yahoo.com>
2019-02-05 12:49:24 +0500

    Complete all Excercise of ch: 2

---
+++ Ch:2 Assignmets .py
@@ -0,0 +1,58 @@
+# Ass no 2.1
+message = "Hellow world!"
+print(message)
+
+# Ass no 2.2
+
+message = "This is the begning of book challanges"
+print(message)
+
+# Ass no 2.3
+
+name = "Bilal Ahmed"
+print("hellow {} would you like to learn some python today".format(name))
+
+#Ass no 2.4
+
+print(name.lower())   #user-name in lower case
+print(name.upper())   #user-name in upper case
+print(name.title())   #user-name in capital form
+
+# Ass no 2.5
+
+author = "Allama Iqbal"
+print("{} once said, \"Nations are born in the hearts of poets,\nthey prosper and die in the hands of politicians.\" ".format(author))
+
+# Ass no 2.6
+
+message = "once said, "
+quote = "\"Nations are born in the hearts of poets,\nthey prosper and die in the hands of politicians.\" "
+print(author,message,quote)
+
+# Ass no 2.7 
+name = "\tBilal Ahmed    "
+print(name)
+
+print(name.lstrip())  #apply left strip method
+print(name.rstrip())  #apply right strip method
+print(name.strip())  #strip method apply    
+
+# Ass no 2.8 
+
+print(5+3)      # addtion operation
+print(10-2)     # subtraction 
+print(2*4)      # multiplication
+print(16//2)     # division
+
+
+# Ass no 2.9 
+
+fav_num = 9
+print(f"My favourite number is {fav_num}")
+
+
+# Ass no 2.10  Adding comment - already using 
+
+# Ch 2 all excercise completed 
+
+
